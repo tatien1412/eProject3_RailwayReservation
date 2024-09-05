d772b31a3e3e5cf0e3383b0f255c76042c8f827a
DucQuy <quy.nd.2259@aptechlearning.edu.vn>
2024-09-05 20:03:14 +0700

    new

--- Back-end/RailwayTransaction/RailwayTransaction/Application/Commands/Admin/UpdateUserCommand.cs
+++ Back-end/RailwayTransaction/RailwayTransaction/Application/Commands/Admin/UpdateUserCommand.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json.Serialization;
 
 namespace RailwayTransaction.Application.Commands.Admin
 {
@@ -10,14 +12,16 @@ namespace RailwayTransaction.Application.Commands.Admin
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
+        public string Role { get; set; }
 
-        public UpdateUserCommand(string fullName, string username, string email, string password, string phoneNumber)
+        public UpdateUserCommand(string fullName, string username, string email, string password, string phoneNumber, string role)
         {
             FullName = fullName;
             UserName = username;
             Email = email;
             Password = password;
             PhoneNumber = phoneNumber;
+            Role = role;
         }
     }
 }
--- Back-end/RailwayTransaction/RailwayTransaction/Handler/Admin/CreateUserCommandHandler.cs
+++ Back-end/RailwayTransaction/RailwayTransaction/Handler/Admin/CreateUserCommandHandler.cs
@@ -2,16 +2,21 @@
 using RailwayTransaction.Application.Commands.Admin;
 using RailwayTransaction.Domain.Entities.Dtos;
 using RailwayTransaction.Domain.Interface;
+using Microsoft.AspNetCore.Identity;  // Thêm thư viện này
 
 namespace RailwayTransaction.Handler.Admin
 {
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserDetail>
     {
         private readonly IRepository<AppUser, string> _repository;
-        public CreateUserCommandHandler(IRepository<AppUser, string> repository)
+        private readonly IPasswordHasher<AppUser> _passwordHasher;  // Thêm IPasswordHasher
+
+        public CreateUserCommandHandler(IRepository<AppUser, string> repository, IPasswordHasher<AppUser> passwordHasher)
         {
             _repository = repository;
+            _passwordHasher = passwordHasher;  // Khởi tạo password hasher
         }
+
         public async Task<UserDetail> Handle(CreateUserCommand command, CancellationToken cancellationToken)
         {
             var appUser = new AppUser
@@ -19,9 +24,11 @@ namespace RailwayTransaction.Handler.Admin
                 FullName = command.FullName,
                 UserName = command.UserName,
                 Email = command.Email,
-                PasswordHash = command.Password,  
                 PhoneNumber = command.PhoneNumber,
             };
+
+            // Sử dụng password hasher để mã hóa mật khẩu
+            appUser.PasswordHash = _passwordHasher.HashPassword(appUser, command.Password);
 
             var addedUser = await _repository.AddAsync(appUser);
 
@@ -31,9 +38,9 @@ namespace RailwayTransaction.Handler.Admin
                 FullName = addedUser.FullName,
                 UserName = addedUser.UserName,
                 Email = addedUser.Email,
-                Password = addedUser.PasswordHash,
+                Password = addedUser.PasswordHash,  // Đây là mật khẩu đã mã hóa
                 PhoneNumber = addedUser.PhoneNumber,
-                Role = command.Role  
+                Role = command.Role
             };
 
             return userDetail;
