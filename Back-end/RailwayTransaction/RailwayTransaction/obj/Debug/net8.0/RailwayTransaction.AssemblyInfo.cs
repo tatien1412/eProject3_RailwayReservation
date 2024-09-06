f8fe7b0468cce7407be2cdf09e8853d5c222b491
TaTien <tien.tn.2280@aptechlearning.edu.vn>
2024-09-06 12:50:07 +0700

    ..

--- Back-end/RailwayTransaction/RailwayTransaction/obj/Debug/net8.0/RailwayTransaction.AssemblyInfo.cs
+++ Back-end/RailwayTransaction/RailwayTransaction/obj/Debug/net8.0/RailwayTransaction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTransaction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3538d007a9ee4539d685941369e55cf24e90690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3124dea8ff559aca1f19ce7a952b2852d57bde1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTransaction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTransaction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
